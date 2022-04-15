710b505d1f40e9f2539a8685de39292d39df2c72
Duc-ju <Trangduc56@gmail.com>
2022-04-15 13:01:45 +0700

    add product meta feld

--- code/store/mobiledevice/serializers.py
+++ code/store/mobiledevice/serializers.py
@@ -5,22 +5,41 @@ class MobileDeviceSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = MobileDevice
-        fields = ['id', 'batteryCapacity', 'warrantyDuration', 'warrantyType', 'screenSize', 'plusSize', 'brand']
+        fields = ['id', 'batteryCapacity', 'warrantyDuration', 'warrantyType', 'screenSize', 'brand']
 
 class LaptopSerializer(serializers.ModelSerializer):
 
+    links = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Laptop
-        fields = ['id', 'batteryCapacity', 'warrantyDuration', 'warrantyType', 'screenSize', 'plusSize', 'brand', 'laptopType', 'storageType', 'weight']
+        fields = ['id', 'links', 'batteryCapacity', 'warrantyDuration', 'warrantyType', 'screenSize', 'brand', 'laptopType', 'storageType', 'weight']
+
+    def get_links(self, obj):
+        return [
+            {
+                "name": "Thiết bị di động",
+                "link": "/mobile-device"
+            },
+            {
+                "name": "Laptop",
+                "link": "/laptop"
+            }
+        ]
 
 class MobilePhoneSerializer(serializers.ModelSerializer):
 
+    type = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = MobilePhone
-        fields = ['id', 'batteryCapacity', 'warrantyDuration', 'warrantyType', 'screenSize', 'plusSize', 'brand', 'ram', 'processorType', 'storageCapacity', 'mobileCableType']
+        fields = ['id', 'type', 'batteryCapacity', 'warrantyDuration', 'warrantyType', 'screenSize', 'brand', 'ram', 'processorType', 'storageCapacity', 'mobileCableType']
+
+    def get_type(self, obj):
+        return "Mobile"
 
 class TabletSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Tablet
-        fields = ['id', 'batteryCapacity', 'warrantyDuration', 'warrantyType', 'screenSize', 'plusSize', 'brand', 'eReader', 'storageCapacity']
+        fields = ['id', 'type', 'batteryCapacity', 'warrantyDuration', 'warrantyType', 'screenSize', 'plusSize', 'brand', 'eReader', 'storageCapacity']
