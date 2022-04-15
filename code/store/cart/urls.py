f7e2459d911ba27c3b916dfaf9f3f49255705900
hieunguyen21122 <hieud18cn1@gmail.com>
2022-04-15 20:52:19 +0700

    get cart by id user, get cart bay id cart, update add produc item to cart

--- code/store/cart/urls.py
+++ code/store/cart/urls.py
@@ -6,4 +6,6 @@ from .views import *
 urlpatterns = [
     path('<int:pk>/cart_products/', CartProductListAPIView.as_view(), name='add_to_cart'),
     path('<int:cart_id>/cart_products/<int:cart_product_id>/', CartProductDetailAPIView.as_view(), name='update_cart'),
+    path('<int:pk>/', CartDetailAPIView.as_view()),
+    path('users/<int:pk>/', CartListAPIView.as_view()),
 ]
